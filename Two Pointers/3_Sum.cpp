7261582af3bb052cea67f8e616aeb37d176dd47d
Sudipta Das <sd1216826@gmail.com>
2025-01-28 21:35:05 +0530

    Create 3_Sum.cpp

---
+++ Two Pointers/3_Sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> answer;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int lo = i + 1, hi = n - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum == 0) {
+                    answer.push_back({nums[i], nums[lo], nums[hi]});
+                    lo++;
+                    hi--;
+                    while (lo < hi && nums[lo] == nums[lo - 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi + 1]) hi--;
+                } else if (sum < 0) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+            }
+        }
+
+        return answer;
+    }
+};
